fd738e980f8b5bed8d7639ec4cf2582a2f3f2fc0
yosef plishtiiv <yosef014@gmail.com>
2022-02-16 19:13:22 +0200

    responsive grid + more im canvas

---
+++ js/main-service.js
@@ -0,0 +1,65 @@
+var gPics = [
+    { id: 1, url: 'img/1.jpg', keywords: ['happy', 'mountain', 'blond'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['trump', 'politics', 'usa'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['animals', 'dogs', 'cute'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['baby', 'funny', 'succes'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['baby', 'dogs', 'funny'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['animals', 'cat'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['happy', 'hat'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['funny', 'baby'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['tv'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['tv'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['tv'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['movies'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['succes', 'funny', 'baby'] },
+    // { id: 14, url: 'img/14.jpg', keywords: ['trump', 'politics', 'usa'] },
+    // { id: 15, url: 'img/15.jpg', keywords: ['happy', 'baby'] },
+    // { id: 16, url: 'img/16.jpg', keywords: ['funny', 'animals', 'dogs'] },
+    // { id: 17, url: 'img/17.jpg', keywords: ['obama', 'politics', 'usa'] },
+    // { id: 18, url: 'img/18.jpg', keywords: ['sport', 'kiss'] },
+    // { id: 19, url: 'img/19.jpg', keywords: ['movies', 'wine'] },
+    // { id: 20, url: 'img/20.jpg', keywords: ['matrix', 'movies'] },
+    // { id: 21, url: 'img/21.jpg', keywords: ['tv'] },
+    // { id: 22, url: 'img/22.jpg', keywords: ['tv', 'funny'] },
+    // { id: 23, url: 'img/23.jpg', keywords: ['movies', 'xman'] },
+    // { id: 24, url: 'img/24.jpg', keywords: ['politics', 'russia', 'putin'] },
+    // { id: 25, url: 'img/25.jpg', keywords: ['movies', 'animation', 'toy story'] }
+];
+
+function craetTextObj(text, pos, size = 50, font = 'Arial') {
+    gTexts.push({
+
+        id: getRandomInt(1, 9999),
+        text,
+        pos,
+        size,
+        font,
+        color: 'black'
+    })
+}
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+function addMouseListeners() {
+    gCanvas.addEventListener('mousemove', onMove)
+    gCanvas.addEventListener('mousedown', onDown)
+    gCanvas.addEventListener('mouseup', onUp)
+    document.addEventListener('keydown', keyType, false)
+}
+
+function addTouchListeners() {
+    gCanvas.addEventListener('touchmove', onMove)
+    gCanvas.addEventListener('touchstart', onDown)
+    gCanvas.addEventListener('touchend', onUp)
+}
+
+function keyType(ev) {
+    if (ev.key === 'Backspace') {
+        gCurrText.text = gCurrText.text.substring(0, gCurrText.text.length - 1)
+    }
+    else { gCurrText.text += ev.key }
+    renderCanvas()
+
+
+}
--- js/main.js
+++ js/main.js
@@ -11,24 +11,14 @@ function init() {
     gCanvas = document.querySelector('.canvas');
     gCtx = gCanvas.getContext('2d');
     addListeners()
-    craetTextObj('press to edit text!', { x: 80, y: 50 })
-    craetTextObj('press to edit text!', { x: 80, y: 350 })
-    gTexts.forEach(text => { drawText(text.text, text.pos, text.size, text.font) });
+    craetTextObj('press to edit text!', { x: 60, y: 50 })
+    craetTextObj('press to edit text!', { x: 60, y: 480 })
+    _firstRender()
     renderPics()
 }
 
-
-
-
-function craetTextObj(text, pos, size = 30, font = 'Arial') {
-    gTexts.push({
-
-        id: getRandomInt(1, 9999),
-        text,
-        pos,
-        size,
-        font
-    })
+function _firstRender() {
+    gTexts.forEach(text => { drawText(text.text, text.pos, text.size, text.font) });
 }
 
 function renderPics() {
@@ -43,43 +33,24 @@ function renderPics() {
 
 function renderCanvas() {
     gCtx.save()
-    // gCtx.fillStvyle = "aqua"
     gCtx.fillRect(0, 0, gCanvas.width, gCanvas.height)
     gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
     gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
-    gTexts.forEach(text => { drawText(text.text, text.pos, text.size, text.font) });
+    gTexts.forEach(text => { drawText(text.text, text.pos, text.size, text.font, text.color) });
     gCtx.restore()
     drawRect()
-
 }
 
-
-function drawText(text, pos, size, font) {
+function drawText(text, pos, size, font, color) {
     var strFont = size + 'px' + ' ' + font
-    gCtx.lineWidth = 1;
-    gCtx.strokeStyle = 'black';
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = color;
     gCtx.fillStyle = '#FFF';
     gCtx.font = strFont;
     gCtx.fillText(text, pos.x, pos.y);
     gCtx.strokeText(text, pos.x, pos.y);
 }
 
-function addListeners() {
-    addMouseListeners()
-    addTouchListeners()
-}
-function addMouseListeners() {
-    gCanvas.addEventListener('mousemove', onMove)
-    gCanvas.addEventListener('mousedown', onDown)
-    gCanvas.addEventListener('mouseup', onUp)
-    document.addEventListener('keydown', keyType, false)
-}
-
-function addTouchListeners() {
-    gCanvas.addEventListener('touchmove', onMove)
-    gCanvas.addEventListener('touchstart', onDown)
-    gCanvas.addEventListener('touchend', onUp)
-}
 
 function onMove(ev) {
     if (isDrag) {
@@ -94,7 +65,6 @@ function onMove(ev) {
 }
 
 function onDown(ev) {
-
     var evpPos = { x: ev.offsetX, y: ev.offsetY } //get pos from ev clicked
     var idxClikedText = gTexts.findIndex(text =>
         evpPos.x >= text.pos.x && evpPos.x <= text.pos.x + gCtx.measureText(text.text).width &&
@@ -115,7 +85,7 @@ function drawRect() {
     var textLengh = gCtx.measureText(gCurrText.text).width
     gCtx.beginPath();
     gCtx.rect(gCurrText.pos.x, gCurrText.pos.y - gCurrText.size, textLengh, gCurrText.size);
-    gCtx.fillStyle = '#ff80004d';
+    gCtx.fillStyle = '#ff80000e';
     gCtx.fillRect(gCurrText.pos.x, gCurrText.pos.y - gCurrText.size, textLengh, gCurrText.size);
     gCtx.strokeStyle = 'black';
     gCtx.stroke();
@@ -127,20 +97,6 @@ function onUp(ev) {
 
 }
 
-function keyType(ev) {
-    console.log(ev.key)
-    if (ev.key === 'Backspace') {
-        gCurrText.text = gCurrText.text.substring(0, gCurrText.text.length - 1)
-        console.log(gCurrText.text.length)
-
-    }
-    else { gCurrText.text += ev.key }
-
-    renderCanvas()
-
-
-}
-
 function incFontSize() {
     gCurrText.size++
     renderCanvas()
@@ -149,12 +105,31 @@ function decFontSize() {
     gCurrText.size--
     renderCanvas()
 }
+function InputColorChange(val) {
+    gCurrText.color = val;
+    renderCanvas()
+}
 
 function onaddText() {
     craetTextObj('press to edit text!', { x: 80, y: getRandomInt(30, 350) })
     renderCanvas()
+}
+function onChangeText() {
+    var currIdx = gTexts.findIndex(text => text.id == gCurrText.id)
+    gCurrText = gTexts[currIdx + 1]
+    if (!gCurrText) gCurrText = gTexts[0]
+    renderCanvas()
 
+}
+function onDeleteText() {
+    var currIdx = gTexts.findIndex(text => text.id == gCurrText.id)
+    gTexts.splice(currIdx, 1)
+    renderCanvas()
+}
+function onStickerClicked(elSticker) {
 
+    craetTextObj(elSticker.innerText, { x: 80, y: getRandomInt(30, 350) })
+    renderCanvas()
 }
 
 function drawImg(elImg) {
@@ -167,34 +142,7 @@ function drawImg(elImg) {
         renderCanvas()
     };
     document.querySelector('.main-container').style.display = "none";
-    document.querySelector('.canva-container').style.display = "block";
+    document.querySelector('.meme-generator').style.display = "flex";
 
 }
 
-var gPics = [
-    { id: 1, url: 'img/1.jpg', keywords: ['happy', 'mountain', 'blond'] },
-    { id: 2, url: 'img/2.jpg', keywords: ['trump', 'politics', 'usa'] },
-    { id: 3, url: 'img/3.jpg', keywords: ['animals', 'dogs', 'cute'] },
-    { id: 4, url: 'img/4.jpg', keywords: ['baby', 'funny', 'succes'] },
-    { id: 5, url: 'img/5.jpg', keywords: ['baby', 'dogs', 'funny'] },
-    { id: 6, url: 'img/6.jpg', keywords: ['animals', 'cat'] },
-    { id: 7, url: 'img/7.jpg', keywords: ['happy', 'hat'] },
-    { id: 8, url: 'img/8.jpg', keywords: ['funny', 'baby'] },
-    { id: 9, url: 'img/9.jpg', keywords: ['tv'] },
-    { id: 10, url: 'img/10.jpg', keywords: ['tv'] },
-    { id: 11, url: 'img/11.jpg', keywords: ['tv'] },
-    { id: 12, url: 'img/12.jpg', keywords: ['movies'] },
-    { id: 13, url: 'img/13.jpg', keywords: ['succes', 'funny', 'baby'] },
-    // { id: 14, url: 'img/14.jpg', keywords: ['trump', 'politics', 'usa'] },
-    // { id: 15, url: 'img/15.jpg', keywords: ['happy', 'baby'] },
-    // { id: 16, url: 'img/16.jpg', keywords: ['funny', 'animals', 'dogs'] },
-    // { id: 17, url: 'img/17.jpg', keywords: ['obama', 'politics', 'usa'] },
-    // { id: 18, url: 'img/18.jpg', keywords: ['sport', 'kiss'] },
-    // { id: 19, url: 'img/19.jpg', keywords: ['movies', 'wine'] },
-    // { id: 20, url: 'img/20.jpg', keywords: ['matrix', 'movies'] },
-    // { id: 21, url: 'img/21.jpg', keywords: ['tv'] },
-    // { id: 22, url: 'img/22.jpg', keywords: ['tv', 'funny'] },
-    // { id: 23, url: 'img/23.jpg', keywords: ['movies', 'xman'] },
-    // { id: 24, url: 'img/24.jpg', keywords: ['politics', 'russia', 'putin'] },
-    // { id: 25, url: 'img/25.jpg', keywords: ['movies', 'animation', 'toy story'] }
-];
